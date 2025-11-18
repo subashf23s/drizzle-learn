c30f6254606d0563a7e1b087cc74f3dcacbb4f6e
Subash <subashsuni55@gmail.com>
2025-11-18 09:48:14 +0530

    added migration

---
+++ src/drizzle/migrations/0001_remarkable_ultragirl.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "users" RENAME COLUMN "name" TO "first_name";--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "last_name" varchar(256);--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "email" varchar(256);
