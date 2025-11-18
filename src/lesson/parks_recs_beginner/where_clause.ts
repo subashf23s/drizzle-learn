cbe05d36e479a735d1bf7c15329f2bb81f7b5af8
Subash <subashsuni55@gmail.com>
2025-11-18 17:01:59 +0530

    updated where clause

--- src/lesson/parks_recs_beginner/where_clause.ts
+++ src/lesson/parks_recs_beginner/where_clause.ts
@@ -1,7 +1,7 @@
 import { sql } from "drizzle-orm";
 import { db } from "../../drizzle/db";
 
-export const whereClause = async () => {
+const employeesalary = async () => {
   //   const data = await db.query.employee_salary.findMany({
   //     where: ({ salary }, { gt }) => gt(salary, 45000),
   //   });
@@ -10,7 +10,24 @@ export const whereClause = async () => {
   //     .from(employee_salary)
   //     .where(gt(employee_salary.salary, 45000));
   const data = await db.query.employeeSalary.findMany({
-    where: ({ salary }) => sql` ${salary} > ${45000} `,
+    where: ({ salary }) => sql` ${salary} >= ${45000} `,
   });
   console.log(data);
+};
+const employeegender = async () => {
+  const data = await db.query.employee_demographics.findMany({
+    where: ({ gender }) => sql` ${gender} != ${"Female"} `,
+  });
+  console.log(data);
+};
+const employeelike = async () => {
+  const data = await db.query.employee_demographics.findMany({
+    where: ({ first_name }) => sql` ${first_name} LIKE ${"J%"} `,
+  });
+  console.log(data);
+};
+export const whereClause = async () => {
+  //   await employeesalary();
+  //   await employeegender();
+  await employeelike();
 };
